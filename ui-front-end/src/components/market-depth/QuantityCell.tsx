68c2c33cd89a01bec11409013109c25d4c7a0f88
tolobayo <tolobayo1@gmail.com>
2024-11-04 20:53:55 -0500

    style background color of bid vs ask quanity cells

--- ui-front-end/src/components/market-depth/QuantityCell.tsx
+++ ui-front-end/src/components/market-depth/QuantityCell.tsx
@@ -1,9 +1,16 @@
 import { PanelRowCellProps } from "./MarketDepthPanelRow";
+import "./QuantityCell.css";
 
 interface QuantityCellProps extends PanelRowCellProps {
   quantity: number;
 }
 
 export const QuantityCell = (props: QuantityCellProps) => {
-  return <td>{props.quantity}</td>;
+  return (
+    <td>
+      <div className={props.column === "ask" ? "AskCell" : "BidCell"}>
+        {props.quantity}
+      </div>
+    </td>
+  );
 };
